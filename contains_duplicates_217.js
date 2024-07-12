01bfefe74961c208031b0f4850cb0f8d1dc27d4c
GeorgeKandelaki <kandelakig29@hotmail.com>
2024-07-12 23:30:19 +0400

    Adding Files

---
+++ contains_duplicates_217.js
@@ -0,0 +1,12 @@
+// 217 COMPLETED 🟩
+
+const containsDuplicate = function (nums) {
+  let seen = new Set();
+  for (let num of nums) {
+    if (seen.has(num)) {
+      return true;
+    }
+    seen.add(num);
+  }
+  return false;
+};
